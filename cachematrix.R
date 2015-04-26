c17b80c00aef1a9a13aa1bc316c041d13eefbe35
Abishek Hari <hariabishek@gmail.com>
2015-04-27 03:27:09 +0530

    Validates if matrix is invertible

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 ## This source file contains 3 functions
 ## 1. makeCacheMatrix - creates a special matrix object to cache its inverse
 ## 2. cacheSolve - will return the cached matrix if exists or builds an inverse otherwise
-## 3. isMatrixInvertible   - will stop processing if the incoming matrix is not a square matrix
+## 3. isMatrixInvertible   - will stop processing if the incoming matrix is not invertible
 
 ## makeCacheMatrix creates a special matrix object that can cache its inverse
 ## input:  matrix
@@ -78,11 +78,15 @@ cacheSolve <- function(x, ...) {
 }
 
 
-## stops the execution if the incoming function cannot be inversed
+## stops the execution if the incoming matrix cannot be inversed
 isMatrixInvertible <- function(x)
 {
     ## stop if number of rows doesnt match with number of columns, i.e its not a square matrix
     if (nrow(x) != ncol(x))
-        stop("matrix is not inversible")
+        stop("matrix is not invertible as the matrix it is a Square Matrix")
+    
+    ## if the determinant of the matrix is 0, then it is not invertible
+    if (det(x) == 0)
+        stop("matrix is not invertible as the matrix it is a singular matrix")
 }
 
